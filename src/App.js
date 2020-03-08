03683900fb226045ec97c1aa8cccef52d606a662
Justin Li <poliworm@gmail.com>
2020-03-08 14:03:22 -0700

    fonts

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="body">
-        <p>helllo</p>
         <Navbar />
       </div>
     );
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,22 +2,24 @@ import React from 'react';
 import './Navbar.css';
 import anime from 'animejs/lib/anime.es.js';
 
-export default class Navbar extends React.Component() {
+export default class Navbar extends React.Component {
     render() {
         return (
             <div className="nav-bar">
-                <div className="logo">
+                <div className = "links">
+                    <ul className = "links">
+                        <li className="link">
+                            about
+                        </li>
+                        <li className="link">
+                            portfolio
+                        </li>
+                        <li className="link">
+                            resume
+                        </li>
+                    </ul>
                 </div>
-                <div className="links">
-                    <div className="link">
-                        <p>about</p>
-                    </div>
-                    <div className="link">
-                        <p>portfoloio</p>
-                    </div>
-                    <div className="link">
-                        <p>resume</p>
-                    </div>
+                <div className="logo">
                 </div>
             </div>
         );
